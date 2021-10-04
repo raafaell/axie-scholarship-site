88be5ae828989db5f23396224b46ff1748e84539
Washington Junior <washingtonjrt@gmail.com>
2021-10-04 20:14:22 -0300

    setup theme

---
+++ src/styles/global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 16px;
+
+    @media (max-width: 414px) {
+      font-size: 15px;
+    }
+  }
+
+
+  body {
+    color: ${(props) => props.theme.colors.main.default};
+    font-family: ${(props) => props.theme.fonts.serif}, sans-serif;
+    font-weight: 400;
+  }
+
+  input, textarea, button {
+    font-family: ${(props) => props.theme.fonts.serif}, sans-serif;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: all 0.2s;
+  }
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    inverse: "#FFFFFF",
+    highlight: "#FFC170",
+    main: {
+      dark: "#462D17",
+      default: "#6D4119",
+    },
+  },
+  fonts: {
+    sansSerif: "Bungee",
+    serif: "Roboto Slab",
+  },
+};
+
+export default theme;
